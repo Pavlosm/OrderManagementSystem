8077e71f38837debf352cecc0d8c401fc665e859
pav <migkiros.p@gmail.com>
2025-02-10 10:05:53 +0200

    last commit before submission

--- src/OrderManagementService.Core/Services/OrderPublisherService.cs
+++ src/OrderManagementService.Core/Services/OrderPublisherService.cs
@@ -1,4 +1,6 @@
-﻿using OrderManagementService.Core.Entities;
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Logging;
+using OrderManagementService.Core.Entities;
 using OrderManagementService.Core.Interfaces;
 using OrderManagementService.Core.Interfaces.Repositories;
 
@@ -7,15 +9,35 @@ namespace OrderManagementService.Core.Services;
 public class OrderPublisherService : IOrderPublisherService
 {
     private readonly IOrderRepository _orderRepository;
+    private readonly ILogger<OrderPublisherService> _logger;
 
-    public OrderPublisherService(IOrderRepository orderRepository)
+    public OrderPublisherService(IOrderRepository orderRepository, ILogger<OrderPublisherService> logger)
     {
         _orderRepository = orderRepository;
+        _logger = logger;
     }
 
     public async Task PublishOrderAsync(OrderDomainEventOutbox eventOutbox)
     {
-        // Publish order to Kafka or RabbitMQ
-        await _orderRepository.DeleteDomainEventAsync(eventOutbox);
+        try
+        {
+            // var connectionsStr = Environment.GetEnvironmentVariable("KAFKA_BOOTSTRAP_SERVERS");
+            // _logger.LogInformation("Kafka bootstrap servers: {0}", connectionsStr);
+            // var config = new ProducerConfig { BootstrapServers = connectionsStr };
+            // using var producer = new ProducerBuilder<Null, string>(config).Build();
+            // var message = new Message<Null, string> { Value = eventOutbox.Message };
+            // var deliveryResult = await producer.ProduceAsync("OrderDomainEvents", message);
+            // _logger.LogInformation("Delivered '{0}' to '{1}' with result {2}", deliveryResult.Value,
+            //     deliveryResult.Topic, deliveryResult.Status);
+            // if (deliveryResult.Status == PersistenceStatus.Persisted)
+            // {
+                await _orderRepository.DeleteDomainEventAsync(eventOutbox);
+            // }
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "An error occurred while publishing the order event: {0}", e.Message);
+        }
+        
     }
 }
